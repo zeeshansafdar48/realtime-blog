e0e2e1cd7454b4d95eebafb2f4e0bb62cf271e26
Zeeshan Safdar <gsoftconsulting01@gmail.com>
2019-08-27 10:08:09 +0500

    Initial commit from gatsby: (https://github.com/HackAfro/gatsby-blog-starter-kit.git)

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: process.env.CI ? `/${name}` : `/`,
+  siteMetadata: {
+    author: 'You!',
+    title: `Gatsby Default (Blog) Starter`,
+  },
+  plugins: [
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/blog`,
+        name: 'blog',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {},
+    },
+    'gatsby-plugin-react-helmet',
+  ],
+};
